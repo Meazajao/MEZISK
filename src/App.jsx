1250df1da038f3f53fc623f4275e71f7b1fecd55
Meazajao <153733325+Meazajao@users.noreply.github.com>
2025-03-19 01:45:42 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import LoginSignup from "./Pages/LoginSignup";
+import Cart from "./Pages/Cart";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/mens" element={<ShopCategory category="mens" />} />
+        <Route path="/womens" element={<ShopCategory category="womens" />} />
+        <Route path="/kids" element={<ShopCategory category="kids" />} />
+        <Route path="/products/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
