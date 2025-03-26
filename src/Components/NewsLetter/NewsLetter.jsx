1308d9a458d906adc3be0c2cb6d435e1e3c64a9d
Meazajao <153733325+Meazajao@users.noreply.github.com>
2025-03-26 13:27:26 +0100

    quick fix

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,16 +1,16 @@
-import styles from './NewsLetter.module.css'
+import styles from './Newsletter.module.css';
 
 const NewsLetter = () => {
   return (
     <div className={styles.newsletter}>
       <h2>Get Exclusive Offers On Your Email</h2>
-      <p>Subscribe to our news letter and stay upated</p>
-      <div>
-        <input type="email" placeholder='Your Email id' />
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div className={styles.form}>
+        <input type="email" placeholder="Your Email ID" />
         <button>Subscribe</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsLetter
+export default NewsLetter;
