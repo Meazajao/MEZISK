e3eca126cd49c1a680f51461fc365ef9a9be66c8
Meazajao <153733325+Meazajao@users.noreply.github.com>
2025-03-27 23:42:06 +0100

    Added footer to all pages and created Context.jsx for upcoming work

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,40 +1,39 @@
-import styles from './Footer.module.css'
-import footer_logo from '../../Assets/logo.png'
-import instagram_icon from '../../Assets/instagram_icon.png'
-import pintres_icon from '../../Assets/pintres_icon.png'
-import whatsapp_icon from '../../Assets/whatsapp_icon.png'
+import styles from './Footer.module.css';
+import footerLogo from '../../Assets/logo.png';
+import instagramIcon from '../../Assets/instagram_icon.png';
+import pinterestIcon from '../../Assets/pinterest_icon.png';
+import whatsappIcon from '../../Assets/whatsapp_icon.png';
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <div className={styles.footer-logo}>
-        <img src={footer_logo} alt="" />
-        <p>MEZISK</p>
-      </div>
-      <ul className={styles.footer-links}>
-        <li>Company</li>
-        <li>Products</li>
-        <li>Offices</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <div className={styles.footer_social_icon}>
-        <div className={styles.footer_icon_container}>
-            <img src={instagram_icon} alt="" />
+    <footer className={styles.footer}>
+      <div className={styles.footerTop}>
+        <div className={styles.logoSection}>
+          <img src={footerLogo} alt="MEZISK Logo" className={styles.logoImage} />
+          <p>MEZISK</p>
         </div>
-        <div className={styles.footer_icon_container}>
-            <img src={pintres_icon} alt="" />
-        </div>
-        <div className={styles.footer_icon_container}>
-            <img src={whatsapp_icon} alt="" />
-        </div>
-      </div>
-      <div className={footer_copyright}>
-        <hr />
-        <p>Copyright @ 2025 - All Right Reserved . </p>
-      </div>
-    </div>
-  )
-}
 
-export default Footer
+        <ul className={styles.navLinks}>
+          <li>Company</li>
+          <li>Products</li>
+          <li>Offices</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+
+        <div className={styles.socials}>
+          <img src={instagramIcon} alt="Instagram" />
+          <img src={pinterestIcon} alt="Pinterest" />
+          <img src={whatsappIcon} alt="WhatsApp" />
+        </div>
+      </div>
+
+      <div className={styles.footerBottom}>
+        <hr />
+        <p>© 2025 MEZISK. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Contex/ShopContex.jsx
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react'
+ 
+
+export const ShopContex = createContext(null)
+
+const ShopContexProvider = (props) => {
+   const contextValue = {
+
+    }
+}
